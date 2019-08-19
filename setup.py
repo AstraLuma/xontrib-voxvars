0847be64a84e2950a2572e74a0f5517dce0cfc47
Jamie Bliss <jamie@ivyleav.es>
2019-08-19 15:36:33 -0400

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='xontrib-voxvars',
-    version='0.0.1',
-    url='https://github.com/astronouth7303/xontrib-voxenv',
-    license='MIT',
-    author='Jamie Bliss',
-    author_email='jamie@ivyleav.es',
-    description='dotenv plus vox',
-    packages=['xontrib'],
-    package_dir={'xontrib': 'xontrib'},
-    package_data={'xontrib': ['*.xsh']},
-    platforms='any',
-    install_requires=[
-        'xonsh',
-    ]
-)
+setup()
